89b22df98cc7f646cab91da7915728fe9e7b62ed
db1-igorsilva <igor.silva@db1.com.br>
2019-07-18 09:03:08 -0300

    New path and const Save = () => System.import('./components/save/Save.vue'); added

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import Save from './components/save/Save.vue';
+// const Save = () => System.import('./components/save/Save.vue');
 import Home from './components/home/Home.vue';
 
 export const routes = [
     { path: '', name: 'home', component: Home, title: "HOME", menu: true },
-    { path: '/save/:id', name: 'save', component: Save, title: "SAVE A NEW PICTURE", menu: true },
+    { path: '/save', name: 'save', component: Save, title: "NEW PICTURE", menu: true },
+    { path: '/save/:id', name: 'update', component: Save, title: "UPDATE PICTURE", menu: false },
     { path: '*', component: Home, menu: false }
 ];
